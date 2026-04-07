c3bd89a1f9dfbd4fee6c6ea5de469e41a51109c9
Hotaku_QMaster <kaznacheev.vanyusha@bk.ru>
2026-04-07 19:23:33 +0500

    fix review code

--- local/templates/delement/components/bitrix/news/reviews/bitrix/news.list/.default/template.php
+++ local/templates/delement/components/bitrix/news/reviews/bitrix/news.list/.default/template.php
@@ -8,12 +8,12 @@
     <!-- Отзыв 1 -->
     <?php foreach($arResult["ITEMS"] as $arItem){ ?>
         <div class="news-item">
-            <?php if (!empty($arResult["DETAIL_PICTURE"]["SRC"]) || !empty($arResult["PREVIEW_PICTURE"]["SRC"])){ ?>
-                <div style="float:left; margin:0 15px 10px 0;">
-                    <a href="<?= $arItem["DETAIL_PAGE_URL"]; ?>">
-                        <img src="<?= !empty($arResult["DETAIL_PICTURE"]["SRC"]) ? $arResult["DETAIL_PICTURE"]["SRC"] : $arResult["PREVIEW_PICTURE"]["SRC"]; ?>" style="border-radius:50%;" />
-                    </a>
-                </div>
+            <?php if (!empty($arItem["DETAIL_PICTURE"]["SRC"]) || !empty($arItem["PREVIEW_PICTURE"]["SRC"])){ ?>
+            <div style="float:left; margin:0 15px 10px 0;">
+                <a href="<?= $arItem["DETAIL_PAGE_URL"]; ?>">
+                    <img src="<?= !empty($arItem["DETAIL_PICTURE"]["SRC"]) ? $arItem["DETAIL_PICTURE"]["SRC"] : $arItem["PREVIEW_PICTURE"]["SRC"]; ?>" width="80" alt="<?php echo $arItem["NAME"]; ?>" style="border-radius:50%;" />
+                </a>
+            </div>
             <?php } ?>
             <div class="news-title">
                 <a href="<?= $arItem["DETAIL_PAGE_URL"]; ?>"><?= $arItem["NAME"]; ?></a>
